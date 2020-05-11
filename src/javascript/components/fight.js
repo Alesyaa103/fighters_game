7bebef58f84e6d4142a00e8c957200f717907c93
Alesya103 <alesya.afanaseva27@gmail.com>
2020-05-11 18:21:18 +0300

    fix issues

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -1,6 +1,4 @@
-import {
-  controls
-} from '../../constants/controls';
+import { controls } from '../../constants/controls';
 
 export async function fight(firstFighter, secondFighter) {
   const {
@@ -11,86 +9,85 @@ export async function fight(firstFighter, secondFighter) {
     PlayerOneCriticalHitCombination,
     PlayerTwoCriticalHitCombination
   } = controls;
-  let FirstPlayerIsAttacking = false;
-  let SecondPlayerIsAttcking = false;
-  let FirstPlayerIsBlocking = false;
-  let SecondPlayerIsBlocking = false
-  let firstFighterHealth = firstFighter.health;
-  let secondFighterHealth = secondFighter.health;
   let firstFighterProgress = document.getElementById('left-fighter-indicator');
   let secondFighterProgress = document.getElementById('right-fighter-indicator');
 
   return new Promise((resolve) => {
+    let FirstPlayerIsAttacking = false;
+    let SecondPlayerIsAttcking = false;
+    let FirstPlayerIsBlocking = false;
+    let SecondPlayerIsBlocking = false;
+    let firstFighterHealth = firstFighter.health;
+    let secondFighterHealth = secondFighter.health;
     let keyPress = []
+
+    function showCurrentHealth(current, basic, el) {
+      let healthPercent = current / basic * 100
+      el.style.width = `${healthPercent > 0 ? healthPercent : 0}%`;
+    }
 
     document.addEventListener('keydown', function (e) {
       keyPress.push(e.code);
+      switch (e.code) {
+        case PlayerOneBlock:
+          FirstPlayerIsBlocking = true;
+          break;
+        case PlayerTwoBlock:
+          SecondPlayerIsBlocking = true;
+          break;
+        case PlayerOneAttack:
+          if (!FirstPlayerIsBlocking && !SecondPlayerIsBlocking) {
+            secondFighterHealth -= getDamage(firstFighter, secondFighter);
+            showCurrentHealth(secondFighterHealth, secondFighter.health, secondFighterProgress);
+          }
+          break;
+        case PlayerTwoAttack:
+          if (!SecondPlayerIsBlocking && !FirstPlayerIsBlocking) {
+            firstFighterHealth -= getDamage(secondFighter, firstFighter)
+            showCurrentHealth(firstFighterHealth, firstFighter.health, firstFighterProgress);
+          }
+          break;
+        default:
+      }
     })
 
     document.addEventListener('keyup', function (e) {
       switch (e.code) {
-        case PlayerOneAttack:
-          if (!FirstPlayerIsBlocking) {
-            setTimeout(() => {
-              if (SecondPlayerIsBlocking) {
-                secondFighter.health -= getDamage(firstFighter, secondFighter);
-              } else {
-                secondFighter.health -= getDamage(firstFighter)
-              }
-              showCurrentHealth(secondFighter.health, secondFighterHealth, secondFighterProgress);
-            }, 500);
-          }
-          break;
-        case PlayerTwoAttack:
-          if (!SecondPlayerIsBlocking) {
-            setTimeout(() => {
-              if (FirstPlayerIsBlocking) {
-                firstFighter.health -= getDamage(secondFighter, firstFighter)
-              } else {
-                firstFighter.health -= getDamage(secondFighter)
-              }
-              showCurrentHealth(firstFighter.health, firstFighterHealth, firstFighterProgress);
-            }, 500);
-          }
-          break;
         case PlayerOneBlock:
-          FirstPlayerIsBlocking = true;
-          setTimeout(() => FirstPlayerIsBlocking = false, 500)
+          FirstPlayerIsBlocking = false;
           break;
         case PlayerTwoBlock:
-          SecondPlayerIsBlocking = true;
-          setTimeout(() => SecondPlayerIsBlocking = false, 500)
+          SecondPlayerIsBlocking = false;
           break;
         default:
       }
 
       if (keyPress.length === 3) {
-        if (keyPress.sort().toString() === PlayerOneCriticalHitCombination.sort().toString() && !FirstPlayerIsAttacking) {
-          secondFighter.health -= firstFighter.attack * 2;
-          showCurrentHealth(secondFighter.health, secondFighterHealth, secondFighterProgress);
+        if (keyPress.sort().join() === PlayerOneCriticalHitCombination.sort().join() && !FirstPlayerIsAttacking) {
+          secondFighterHealth -= firstFighter.attack * 2;
+          showCurrentHealth(secondFighterHealth, secondFighter.health, secondFighterProgress);
           FirstPlayerIsAttacking = true;
           setTimeout(() => FirstPlayerIsAttacking = false, 10000);
         }
-        if (keyPress.sort().toString() === PlayerTwoCriticalHitCombination.sort().toString() && !SecondPlayerIsAttcking) {
-          firstFighter.health -= secondFighter.attack * 2;
-          showCurrentHealth(firstFighter.health, firstFighterHealth, firstFighterProgress);
+        if (keyPress.sort().join() === PlayerTwoCriticalHitCombination.sort().join() && !SecondPlayerIsAttcking) {
+          firstFighterHealth -= secondFighter.attack * 2;
+          showCurrentHealth(firstFighterHealth, firstFighter.health, firstFighterProgress);
           SecondPlayerIsAttcking = true;
           setTimeout(() => SecondPlayerIsAttcking = false, 10000)
         }
       }
+
       keyPress = [];
-      if (firstFighter.health <= 0 || secondFighter.health <= 0) {
-        resolve(firstFighter.health > 0 ? firstFighter : secondFighter);
+
+      if (firstFighterHealth <= 0 || secondFighterHealth <= 0) {
+        resolve(firstFighterHealth > 0 ? firstFighter : secondFighter);
       }
     })
   });
 }
 
 export function getDamage(attacker, defender) {
-  let damage = getHitPower(attacker)
-  if (defender) {
-    damage -= getBlockPower(defender)
-  }
+  let damage = getHitPower(attacker) - getBlockPower(defender)
   return damage < 0 ? 0 : damage;
 }
 
@@ -100,8 +97,4 @@ export function getHitPower(fighter) {
 
 export function getBlockPower(fighter) {
   return fighter.defense * (Math.random() + 1)
-}
-
-function showCurrentHealth(current, basic, el) {
-  el.style.width = `${current/basic*100}%`
 }
